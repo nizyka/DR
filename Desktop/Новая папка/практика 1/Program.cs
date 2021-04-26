28ab139210239b7658f92716580002c0c0353c19
nizyka <anya.kyazimova@bk.ru>
2021-04-26 23:37:26 +0300

    baam

---
+++ Desktop/Новая папка/практика 1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace практика_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=1;
+            Console.Write("Введите значение a= ");
+            int a = int.Parse(Console.ReadLine());
+            if (a < 5)
+            {
+                i++;
+            }
+            Console.Write("Введите  b= ");
+            int b = int.Parse(Console.ReadLine());
+            if (b < 5)
+            {
+                i++;
+            }
+            Console.Write("Введите c= ");
+            int c = int.Parse(Console.ReadLine());
+            if (c < 5)
+            {
+                i++;
+            }
+            if (i == 2)
+            {
+                Console.Write("yes");
+            }
+            else
+            {
+                Console.Write("no");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
